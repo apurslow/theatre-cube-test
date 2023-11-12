69ce32e59a27472f960135deee77da091fdb74a5
apurslow <aaronp26@uw.edu>
2023-11-12 15:28:46 -0800

    initial theatre setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas, useFrame } from "@react-three/fiber";
+import { ScrollControls, useScroll, Box, Sky, Cylinder } from "@react-three/drei";
+import { getProject, val } from "@theatre/core";
+import { SheetProvider, PerspectiveCamera, useCurrentSheet, editable } from "@theatre/r3f";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const sheet = getProject('Bouncy Box').sheet('Scene')
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
+      <ScrollControls pages={1} damping={0.5} maxSpeed={0.5}>
+        <SheetProvider sheet={sheet}>
+          <Scene />
+        </SheetProvider>
+      </ScrollControls>
+    </Canvas>
   )
 }
 
 export default App
+
+function Scene() {
+
+  const sheet = useCurrentSheet()
+  const scroll = useScroll()
+
+  //our callback will run on every animation frame
+  useFrame(() => {
+
+    //the length of our sequence
+    const sequenceLength = val(sheet.sequence.pointer.length)
+
+    //update the "position" of trhe playhead in the  sequence as a fraction of the whole length
+    sheet.sequence.position = scroll.offset * sequenceLength
+
+  })
+  return (
+    <>
+      <color attach='background' args={['skyblue']} />
+      <Sky sunPosition={[100, 20, 100]}  />
+      <ambientLight intensity={0.2} />
+      <editable.pointLight theatreKey="Light" position={[10, 10, 10]} />
+      <editable.mesh  theatreKey="Box">
+      <Box >
+        <meshStandardMaterial color="hotpink" />
+      </Box>
+      </editable.mesh>
+      <editable.mesh  theatreKey="Cylinder">
+      <Cylinder >
+        <meshStandardMaterial color="forestgreen" />
+      </Cylinder>
+      </editable.mesh>
+      <PerspectiveCamera
+        theatreKey="Camera"
+        makeDefault
+        position={[0, 0, 5]}
+        fov={90}
+        near={0.1}
+        far={70}
+      />
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Suspense } from 'react'
+
+// **Studio toggle here**
+import studio from '@theatre/studio'
+import extension from '@theatre/r3f/dist/extension'
+
+studio.extend(extension);
+studio.initialize();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
 )
